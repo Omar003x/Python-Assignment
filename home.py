08d67df5026f038354fc4ab7fe97184fd84fce91
Omar003x <154488216+Omar003x@users.noreply.github.com>
2024-01-29 16:11:42 +0800

    home.py

--- home.py
+++ home.py
@@ -1,199 +1,114 @@
-# Import All Pages #
+# Import necessary modules
 from pages.student import studentPage
 from pages.lecturer import lecturerPage
 from pages.admin import adminPage
 from pages.trainer import trainerPage
-
-# Importing OS to Clear Terminal (line 172) #
 import os
-
-# Importing JSON to Read Data from Text File #
 import json
-
-# Importing Getpass to Mask User Password Input #
 import getpass
+import datetime 
 
-import datetime
+# Function to load data from file
+def loadData():
+    with open("data.txt", "r") as db:
+        return json.load(db)
 
-# Read Data Text File with JSON #
-db = open("data.txt", "r")
-data = json.load(db)
-
+# Function to update data in file
 def dataUpdater(data):
-    # Overwriting Data #
-    data = json.dumps(data, indent=2)
+    with open("data.txt", "w") as db:
+        json.dump(data, db, indent=2)
+    print("Account successfully created!")
 
-    # Updating Data in Text File # 
-    db = open("data.txt", "w")
-    db.write(data)
-
-    # Message # 
-    print(f"Account successfully created!")
-
-    # Reloading Data #
-    db = open("data.txt", "r")
-    data = json.load(db)
-
-# Home #
-def homePage():
-
-    # Home Page Welcoming #
+# Home Page
+def homePage(data):
     print("Welcome to APU Café")
-
-    # Home Page Navigator #
     while True:
         homeMenuChoice = input("1. Log in \n2. Sign up\n3. Exit\n")
+        if homeMenuChoice == '1':
+            logIn(data)
+        elif homeMenuChoice == '2':
+            signUp(data)
+        elif homeMenuChoice == '3':
+            print("Thanks for using APU Café!")
+            break
+        else:
+            print("Invalid choice!")
 
-        match homeMenuChoice:
-            case '1' :
-                logIn()
-            case '2' :
-                signUp()
-            case '3' :
-                print("Thanks for using APU Café!")
-                break
-            case other:
-                print("Invalid choice!")          
-
-# Sign Up #
-def signUp():
-    # Make Local Data Accessible #
-    global data
-    
+# Sign Up Function
+def signUp(data):
     print("Creating an account")
+    userTPNumber = input("Enter your TP number (TPxxxxxx): ")
+    if not userTPNumber.startswith("TP") or not userTPNumber[2:].isdigit() or len(userTPNumber) != 8:
+        print("Invalid TP Number!")
+        return
 
-    # TP Number #
-    userValid = False
+    for user in data["users_data"]:
+        if userTPNumber == user["user_tp"]:
+            print("TP number is already existing!")
+            return
 
-    while userValid == False:
-     
-        userTPNumber = str(input("Enter your TP number: TP"))
-        
-        if not userTPNumber.isnumeric or not len(str(userTPNumber)) == 6:
-            print("Invalid TP Number!")
-        else:
-            for user in data["users_data"]:
-                if f"TP{userTPNumber}" == user["user_tp"]:
-                    print("TP number is already existing!")
-                    return
-            else:
-                userValid = True
-                              
-     
-    # Name #
-    while True:
-      userFirstName = input("Enter your First name: ")
-      userLastName = input("Enter your Last name: ")
+    userFirstName = input("Enter your First name: ").capitalize()
+    userLastName = input("Enter your Last name: ").capitalize()
+    userName = f"{userFirstName} {userLastName}"
 
-      if userFirstName.isalpha() and userLastName.isalpha():
-          break
-      else:
-          print("Name includes unallowed characters!")
-      
-    userName = (f"{userFirstName.capitalize()} {userLastName.capitalize()}")
-    
-    # Password #
-    while True:
-        userPassword = getpass.getpass(prompt="Enter your Password: ")
-        userPasswordConfirm = getpass.getpass(prompt="Confirm your Password: ")
+    userPassword = getpass.getpass(prompt="Enter your Password: ")
+    userPasswordConfirm = getpass.getpass(prompt="Confirm your Password: ")
+    if userPassword != userPasswordConfirm:
+        print("Passwords do not match!")
+        return
+    if len(userPassword) < 8:
+        print("Password too short!")
+        return
 
-        if userPassword == userPasswordConfirm:
-            if len(userPassword) < 8:
-                print("Password too short!")
-            else:
-                break
-        else:
-            print("Password does not match!")
+    userRole = input("Choose your role (1. Admin, 2. Lecturer, 3. Trainer, 4. Student): ")
+    roleDict = {'1': 'admin', '2': 'lecturer', '3': 'trainer', '4': 'student'}
+    userRole = roleDict.get(userRole, 'unassigned')
+    if userRole == 'unassigned':
+        print("Invalid choice!")
+        return
 
-    # User Role Assigner #      
-    while True:
-        userRoleChoice = input("1. Admin \n2.Lecturer \n3.Trainer \n4.Student\n")
-
-        userRole = "unassigned"
-
-        match userRoleChoice:
-            case '1' :
-                userRole = "admin"
-                break
-            case '2' :
-                userRole = "lecturer"
-                break
-            case '3' :
-                userRole = "trainer"
-                break
-            case '4' :
-                userRole = "student"
-                break
-            case other:
-                print("Invalid choice!")
-
-    currentDate = datetime.date.today()
-
-    # New User Formatting #
-    new_user = {"user_tp":f"TP{userTPNumber}", "password":userPassword, "fullname":userName, "role":userRole, "creation_date":f"{currentDate}"}
-
-    # Adding the User to Database # 
+    currentDate = str(datetime.date.today())
+    new_user = {
+        "user_tp": userTPNumber,
+        "password": userPassword,
+        "fullname": userName,
+        "role": userRole,
+        "creation_date": currentDate
+    }
     data["users_data"].append(new_user)
-
     dataUpdater(data)
 
-# Log in # 
-def logIn():
-    # Make Local Data Accessible #
-    global data
-    
-    # In Case No User is Assigned #
-    if len(data["users_data"]) == 0:
+# Log In Function
+def logIn(data):
+    if not data["users_data"]:
         print("No users assigned! Please sign up.")
-    else:
-        # Setting Variables #
-        attempts = 3
-        loggedIn = False
+        return
 
-        # Loop to Compare Entered Credentials to Database #
-        while (attempts + 1) > 0 and loggedIn == False :
-                
-                # User Input #
-                print("Kindly Log In")
-                enteredTP = input("TP number: TP")
-                enteredPasskey = getpass.getpass(prompt="Password: ")
-                for user in data["users_data"]:
+    attempts = 3
+    while attempts > 0:
+        enteredTP = input("TP number: TP")
+        enteredPasskey = getpass.getpass(prompt="Password: ")
+        for user in data["users_data"]:
+            if user["user_tp"] == f"TP{enteredTP}" and user["password"] == enteredPasskey:
+                print("Successful log in!")
+                name = user["fullname"].split()[0]
+                print(f"Hello, {name}!")
+                roleFunction = {
+                    "admin": adminPage,
+                    "lecturer": lecturerPage,
+                    "trainer": trainerPage,
+                    "student": lambda: studentPage(user, data, dataUpdater)
+                }
+                roleFunction.get(user["role"], lambda: print("Role unassigned! Please contact an APU Café admin."))()
+                return
+        attempts -= 1
+        if attempts:
+            print(f"Invalid TP number or password! {attempts} attempts left")
+        else:
+            print("Too many attempts, try again later.")
+            break
 
-
-                    if user["user_tp"] == (f"TP{enteredTP}") and user["password"] == enteredPasskey:
-
-                        print("successful log in!")
-                        loggedIn = True
-                    
-                        name = user["fullname"].split()[0]
-                        print(f"Hello, {name}!")
-
-                        # Redirect to Page Respective to User Role #
-                        match user["role"]:
-                            case "admin":
-                                adminPage()
-                                break
-                            case "trainer":
-                                trainerPage()
-                                break
-                            case "student":
-                                studentPage(user, data, dataUpdater)
-                                break
-                            case "lecturer":
-                                lecturerPage ()
-                                break
-                            case other:
-                                print("Role unassigned! Please contact an APU Café admin.")
-                
-                # Attempts Counter #
-                else:
-                    if attempts == 0 :
-                        print("Too much attempts, try again later.")
-                        break
-                    else:
-                        os.system('cls')
-                        print(f"Invalid TP number or password! {attempts} attempts left")
-                        attempts-=1
-                                     
-# Home Page Initiator #
-homePage()
+# Main
+if __name__ == "__main__":
+    data = loadData()
+    homePage(data)
